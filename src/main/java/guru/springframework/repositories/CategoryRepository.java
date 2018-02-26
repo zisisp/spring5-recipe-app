21403e5f6c906cfdecedb0f995869c46c2ae6957
Zisis Pontikas <Zisis.Pontikas@amdocs.com>
2018-02-26 07:59:07 +0200

    crud

---
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<CategoryRepository,Long>{
+
+}
